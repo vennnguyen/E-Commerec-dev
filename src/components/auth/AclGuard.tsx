7b7bfc55f8384b8c09f6b6f12749fdee0a876056
atuan41005 <tuan100205@gmail.com>
2025-09-15 10:43:10 +0700

    phân quyền cơ bản bằng @casl/ability

--- src/components/auth/AclGuard.tsx
+++ src/components/auth/AclGuard.tsx
@@ -2,7 +2,12 @@
 import { ReactNode } from 'react'
 
 // ** Types
-import type { ACLObj } from 'src/configs/acl'
+import { AppAbility, buildAbilityFor, type ACLObj } from 'src/configs/acl'
+import BlankLayout from 'src/view/layout/BlankLayout'
+import NotAuthorized from 'src/pages/401'
+import { useAuth } from 'src/hooks/useAuth'
+import { useRouter } from 'next/router'
+import { AbilityContext } from '../acl/Can'
 
 interface AclGuardProps {
   children: ReactNode
@@ -14,7 +19,32 @@ interface AclGuardProps {
 const AclGuard = (props: AclGuardProps) => {
   // ** Props
   const { aclAbilities, children, guestGuard = false, authGuard = true } = props
-  return <>{children}</>
+  const auth = useAuth()
+  const router = useRouter()
+  let ability: AppAbility
+  const permissionUser = auth.user?.role.permissions ?? []
+  if (auth.user && !ability) {
+    ability = buildAbilityFor(permissionUser, aclAbilities.subject)
+  }
+  //trang khôn khả dụng hoặc chưa đăng nhập
+  if (guestGuard || router.route === '/500' || router.route === '/404' || !authGuard) {
+    if (auth.user && ability) {
+      return <AbilityContext.Provider value={ability}>{children}</AbilityContext.Provider>
+    } else {
+      return <>{children}</>
+    }
+  }
+  //check quyền khi đã đăng nhập
+  if (ability && auth.user && ability.can(aclAbilities.action, aclAbilities.subject)) {
+    return <AbilityContext.Provider value={ability}>{children}</AbilityContext.Provider>
+  }
+  return (
+    <>
+      <BlankLayout>
+        <NotAuthorized />
+      </BlankLayout>
+    </>
+  )
 }
 
 export default AclGuard
