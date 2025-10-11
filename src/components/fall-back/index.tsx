f876f6c62a8a344704e9db0cdac09e0ca9566d8b
atuan41005 <tuan100205@gmail.com>
2025-10-11 15:45:14 +0700

    đổi config never read của eslint

--- src/components/fall-back/index.tsx
+++ src/components/fall-back/index.tsx
@@ -1,11 +1,10 @@
 // ** MUI Imports
-import { useTheme } from '@mui/material/styles'
+
 import Box, { BoxProps } from '@mui/material/Box'
 import CircularProgress from '@mui/material/CircularProgress'
 
 const FallbackSpinner = ({ sx }: { sx?: BoxProps['sx'] }) => {
   // ** Hook
-  const theme = useTheme()
 
   return (
     <Box
